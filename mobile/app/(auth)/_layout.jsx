794da463ef0c6d2b91ea4c8848da2e8e39d7cf94
soumadip-dev <soumadipmajila@gmail.com>
2025-01-02 08:30:17 +0530

    feat: created (auth)/-layout.jsx

--- mobile/app/(auth)/_layout.jsx
+++ mobile/app/(auth)/_layout.jsx
@@ -1,10 +1,10 @@
-import { useAuth } from '@clerk/clerk-expo';
 import { Redirect, Stack } from 'expo-router';
-const AuthRoutesLayout = () => {
+import { useAuth } from '@clerk/clerk-expo';
+
+export default function AuthRoutesLayout() {
   const { isSignedIn } = useAuth();
-  if (isSignedIn) {
-    return <Redirect href={'/'} />;
-  }
-  return <Stack />;
-};
-export default AuthRoutesLayout;
+
+  if (isSignedIn) return <Redirect href={'/'} />;
+
+  return <Stack screenOptions={{ headerShown: false }} />;
+}

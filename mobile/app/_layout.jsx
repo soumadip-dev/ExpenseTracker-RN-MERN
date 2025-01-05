4e239b2c472d5852f742873207b3d44205cbe0af
soumadip-dev <soumadipmajila@gmail.com>
2025-01-05 08:15:22 +0530

    feat: Add <ClerkProvider> to root layout

--- mobile/app/_layout.jsx
+++ mobile/app/_layout.jsx
@@ -1,10 +1,13 @@
 import { Stack } from 'expo-router';
 import SafeScreen from '../components/SafeScreen';
+import { ClerkProvider } from '@clerk/clerk-expo';
 
 export default function RootLayout() {
   return (
-    <SafeScreen>
-      <Stack />
-    </SafeScreen>
+    <ClerkProvider>
+      <SafeScreen>
+        <Stack />
+      </SafeScreen>
+    </ClerkProvider>
   );
 }

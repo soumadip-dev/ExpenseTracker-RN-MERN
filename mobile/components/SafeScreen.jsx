20628a6170df40d369a7acb0a89460662c0fd02a
soumadip-dev <soumadipmajila@gmail.com>
2025-01-05 16:55:29 +0530

    fix: resolve error in SafeScreen component

--- mobile/components/SafeScreen.jsx
+++ mobile/components/SafeScreen.jsx
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { COLORS } from '../constants/colors.js';
 

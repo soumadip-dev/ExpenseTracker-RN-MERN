277981fac43221136e05aad956dd9be74ca9585b
soumadip-dev <soumadipmajila@gmail.com>
2025-01-05 14:25:54 +0530

    refactor: clean up index.jsx and set as default using rnfe

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,34 +1,9 @@
-import { Image } from 'expo-image';
-import { Link } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function Index() {
+import { View, Text } from 'react-native';
+const index = () => {
   return (
-    <View style={styles.container}>
-      <Text style={{ fontSize: 20, color: 'blue' }}>starting the app...</Text>
-      <Link href={'/about'} style={{ fontSize: 20, color: 'red' }}>
-        About
-      </Link>
-
-      <Image
-        source={{
-          uri: 'https://plus.unsplash.com/premium_photo-1755534520276-7a75d7b70915?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8',
-        }}
-        style={{ width: 200, height: 200 }}
-      />
-
-      <Image
-        source={require('@/assets/images/react-logo.png')}
-        style={{ width: 200, height: 200 }}
-      />
+    <View>
+      <Text>index</Text>
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
+export default index;

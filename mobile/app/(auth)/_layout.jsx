c24a296881addfecbd73ab6f0a6923d3ed936117
soumadip-dev <soumadipmajila@gmail.com>
2025-01-05 09:27:45 +0530

    feat: add AuthRoutesLayout to handle routing based on Clerk authentication

---
+++ mobile/app/(auth)/_layout.jsx
@@ -0,0 +1,10 @@
+import { useAuth } from '@clerk/clerk-expo';
+import { Redirect, Stack } from 'expo-router';
+const AuthRoutesLayout = () => {
+  const { isSignedIn } = useAuth();
+  if (isSignedIn) {
+    return <Redirect href={'/'} />;
+  }
+  return <Stack />;
+};
+export default AuthRoutesLayout;

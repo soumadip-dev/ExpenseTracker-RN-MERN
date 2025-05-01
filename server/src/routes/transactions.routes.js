e8cf53761a427244fdada7c91e30bd1e88c24ccc
soumadip-dev <soumadipmajila@gmail.com>
2025-05-01 11:47:12 +0530

    feat: add createTransaction controller to routes

--- server/src/routes/transactions.routes.js
+++ server/src/routes/transactions.routes.js
@@ -1,5 +1,8 @@
 import { Router } from 'express';
+import { createTransaction } from '../controllers/transactions.controller.js';
 
 const router = Router();
+
+router.post('/', createTransaction);
 
 export default router;

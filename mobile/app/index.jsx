aad9e92e5a61f2213b0a1402783cfb266fecae4a
soumadip-dev <soumadipmajila@gmail.com>
2025-05-15 09:25:33 +0530

    chore: practice basic React Native Expo setup and components

--- mobile/app/index.jsx
+++ mobile/app/index.jsx
@@ -1,3 +1,5 @@
+import { Image } from 'expo-image';
+import { Link } from 'expo-router';
 import { Text, View } from 'react-native';
 
 export default function Index() {
@@ -9,7 +11,22 @@ export default function Index() {
         alignItems: 'center',
       }}
     >
-      <Text>starting the app...</Text>
+      <Text style={{ fontSize: 20, color: 'blue' }}>starting the app...</Text>
+      <Link href={'/about'} style={{ fontSize: 20, color: 'red' }}>
+        About
+      </Link>
+
+      <Image
+        source={{
+          uri: 'https://plus.unsplash.com/premium_photo-1755534520276-7a75d7b70915?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwxfHx8ZW58MHx8fHx8',
+        }}
+        style={{ width: 200, height: 200 }}
+      />
+
+      <Image
+        source={require('@/assets/images/react-logo.png')}
+        style={{ width: 200, height: 200 }}
+      />
     </View>
   );
 }

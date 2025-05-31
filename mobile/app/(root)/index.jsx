0d3d0d0b08bf145a8431ff88ccc66eefa3459ae2
soumadip-dev <soumadipmajila@gmail.com>
2025-05-31 09:15:33 +0530

    feat: create header part of index.jsx

--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -1,10 +1,12 @@
 import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo';
 import { Link } from 'expo-router';
-import { Text, View } from 'react-native';
+import { Alert, FlatList, Image, RefreshControl, Text, TouchableOpacity, View } from 'react-native';
 import { SignOutButton } from '@/components/SignOutButton';
 import { useTransactions } from '../../hooks/useTransactions.js';
 import { useEffect } from 'react';
 import PageLoader from '../../components/PageLoader.jsx';
+import { styles } from '../../assets/styles/home.styles.js';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function Page() {
   const { user } = useUser();
@@ -23,19 +25,32 @@ export default function Page() {
   console.log('User ID', user?.id);
 
   return (
-    <View>
-      <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-        <SignOutButton />
-      </SignedIn>
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </SignedOut>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        <View style={styles.header}>
+          {/* LEFT */}
+          <View style={styles.headerLeft}>
+            <Image
+              source={require('../../assets/images/logo.png')}
+              style={styles.headerLogo}
+              resizeMode="contain"
+            />
+            <View style={styles.welcomeContainer}>
+              <Text style={styles.welcomeText}>Welcome,</Text>
+              <Text style={styles.usernameText}>
+                {user?.emailAddresses[0]?.emailAddress.split('@')[0]}
+              </Text>
+            </View>
+          </View>
+          {/* RIGHT */}
+          <View style={styles.headerRight}>
+            <TouchableOpacity style={styles.addButton} onPress={() => router.push('/create')}>
+              <Ionicons name="add" size={20} color="#FFF" />
+              <Text style={styles.addButtonText}>Add</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
     </View>
   );
 }

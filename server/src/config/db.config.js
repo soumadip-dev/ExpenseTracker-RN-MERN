c11afb55c97278a5be144eae48d31696558963b4
soumadip-dev <soumadipmajila@gmail.com>
2025-08-15 23:39:41 +0530

    chore: update env.config.js to load DATABASE_URL and create db.config.js with Neon database connection

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -1,11 +1,3 @@
-// import { neon } from '@neondatabase/serverless';
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// const sql = neon(process.env.DATABASE_URL);
-
-// export default sql;
-
 import { neon } from '@neondatabase/serverless';
 import { ENV } from './env.config';
 
--- server/src/config/env.config.js
+++ server/src/config/env.config.js
@@ -4,4 +4,5 @@ dotenv.config();
 
 export const ENV = {
   PORT: process.env.PORT,
+  DATABASE_URL: process.env.DATABASE_URL,
 };

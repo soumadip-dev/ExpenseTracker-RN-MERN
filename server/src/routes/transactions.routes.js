eba40d5fc3800a77b108beacb1fca6efa126e877
soumadip-dev <soumadipmajila@gmail.com>
2025-08-17 19:25:30 +0530

    feat: add route for deleting a transaction

--- server/src/routes/transactions.routes.js
+++ server/src/routes/transactions.routes.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import {
   createTransaction,
   getTransactionsByUserId,
+  deleteTransaction,
 } from '../controllers/transactions.controller.js';
 
 const router = Router();
 
 router.post('/', createTransaction);
 router.get('/:userId', getTransactionsByUserId);
+router.delete('/:transactionId', deleteTransaction);
 
 export default router;

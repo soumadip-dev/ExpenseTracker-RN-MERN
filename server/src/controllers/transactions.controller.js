a34cf8bf95231dbc5237f6555605e4b0b7b0b619
soumadip-dev <soumadipmajila@gmail.com>
2025-08-17 19:25:08 +0530

    feat: add controller to delete a transaction

--- server/src/controllers/transactions.controller.js
+++ server/src/controllers/transactions.controller.js
@@ -1,3 +1,5 @@
+import mongoose from 'mongoose';
+import Transaction from '../model/transactions.model.js';
 import {
   createTransactionService,
   getTransactionsByUserIdService,
@@ -55,4 +57,35 @@ const getTransactionsByUserId = async (req, res) => {
   }
 };
 
-export { createTransaction, getTransactionsByUserId };
+//* Controller to delete a transaction
+const deleteTransaction = async (req, res) => {
+  try {
+    // Get transactionId
+    const { transactionId } = req.params;
+
+    // Check if transactionId is present and valid
+    if (!transactionId || !mongoose.Types.ObjectId.isValid(transactionId)) {
+      return res.status(400).json({ message: 'Invalid transactionId', success: false });
+    }
+
+    // Delete transaction
+    const result = await Transaction.findByIdAndDelete(transactionId);
+
+    // Check if transaction is deleted
+    if (!result) {
+      return res.status(404).json({ message: 'Transaction not found', success: false });
+    }
+
+    // Send response (success)
+    res.status(200).json({
+      message: 'Transaction deleted successfully',
+      data: result,
+      success: true,
+    });
+  } catch (error) {
+    console.error('Error getting transactions', error);
+    // Send response (error)
+    res.status(500).json({ message: 'Internal server error', success: false });
+  }
+};
+export { createTransaction, getTransactionsByUserId, deleteTransaction };

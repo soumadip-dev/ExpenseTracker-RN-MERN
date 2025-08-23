891bb729b51e55a175faadcb5cc1542a17f7123e
soumadip-dev <soumadipmajila@gmail.com>
2025-08-23 16:56:49 +0530

    feat: implement handleSubmit function in create.jsx to call API

--- mobile/app/(root)/create.jsx
+++ mobile/app/(root)/create.jsx
@@ -1,4 +1,8 @@
-import { View, Text } from 'react-native';
+import { View, Text, Alert } from 'react-native';
+import { useRouter } from 'expo-router';
+import { useUser } from '@clerk/clerk-expo';
+import { useState } from 'react';
+import { API_URL } from '../../constants/api';
 
 const CATEGORIES = [
   { id: 'food', name: 'Food & Drinks', icon: 'fast-food' },
@@ -11,6 +15,60 @@ const CATEGORIES = [
 ];
 
 const CreateScreen = () => {
+  const router = useRouter();
+  const { user } = useUser();
+
+  const [title, setTitle] = useState('');
+  const [amount, setAmount] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('');
+  const [isExpense, setIsExpense] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleCreate = async () => {
+    // validations
+    if (!title.trim()) return Alert.alert('Error', 'Please enter a transaction title');
+    if (!amount || isNaN(parseFloat(amount)) || parseFloat(amount) <= 0) {
+      Alert.alert('Error', 'Please enter a valid amount');
+      return;
+    }
+
+    if (!selectedCategory) return Alert.alert('Error', 'Please select a category');
+
+    setIsLoading(true);
+    try {
+      // Format the amount (negative for expenses, positive for income)
+      const formattedAmount = isExpense
+        ? -Math.abs(parseFloat(amount))
+        : Math.abs(parseFloat(amount));
+
+      const response = await fetch(`${API_URL}/transactions`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          user_id: user.id,
+          title,
+          amount: formattedAmount,
+          category: selectedCategory,
+        }),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        console.log(errorData);
+        throw new Error(errorData.error || 'Failed to create transaction');
+      }
+
+      Alert.alert('Success', 'Transaction created successfully');
+      router.back();
+    } catch (error) {
+      Alert.alert('Error', error.message || 'Failed to create transaction');
+      console.error('Error creating transaction:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <View>
       <Text>CreateScreen</Text>
